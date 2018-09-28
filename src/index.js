990170cfd0fd91f773bea04452b984ec6770aa28
mxwoodie <maxswation@gmail.com>
2018-09-28 20:58:11 +0300

    brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  
+  let temp = str;
+
+  // if (str.length % 2 !== 0) {
+  //   return false;
+  // }
+
+  for (let i = 0; i < temp.length; i++) {
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      if (temp[i] === bracketsConfig[j][0] && temp[i+1] === bracketsConfig[j][1]) {
+        temp =  temp.slice(0, i) + temp.slice(i+2, temp.length);
+        i = 0;
+      }
+    }
+  }
+
+  if (!temp.length) {
+    return true;
+  } else return false;
+  
 }
