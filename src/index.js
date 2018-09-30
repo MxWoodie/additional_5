596b32757c635f233c1a8d4d4b483a9a4d7d36a4
mxwoodie <maxswation@gmail.com>
2018-09-30 16:59:40 +0300

    fixed

--- src/index.js
+++ src/index.js
@@ -15,6 +15,15 @@ module.exports = function check(str, bracketsConfig) {
     }
   }
 
+  for (let i = 0; i < temp.length; i++) {
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      if (temp[i] === bracketsConfig[j][0] && temp[i+1] === bracketsConfig[j][1]) {
+        temp =  temp.slice(0, i) + temp.slice(i+2, temp.length);
+        i = 0;
+      }
+    }
+  }
+
   if (!temp.length) {
     return true;
   } else return false;
